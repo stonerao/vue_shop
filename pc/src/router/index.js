622c2c09977e6b2b4ec85bd8ef79a31225a0691d
stone <674656681@QQ.com>
2018-01-23 10:28:06 +0800

    router Signed-off-by: stone <674656681@QQ.com>

--- pc/src/router/index.js
+++ pc/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import * as whileList from './while'
+import * as needList from './need'
 Vue.use(Router);
 // 路由列表
-const routers = [...[{
-    path: '/',
-    name: 'index',
-    component: r => require.ensure([], () => r(require('@/view/index/index')))
-}], ...whileList.list];
-// 导出参数
+const routerList = [{
+        path: '/',
+        name: 'index',
+        component: r => require.ensure([], () => r(require('@/view/index/index')))
+    }]
+    // 导出参数
 export default new Router({
-    routes: routers,
+    mode: 'history',
+    routes: [...routerList, ...whileList.router, ...needList.router], //所有路由
     linkActiveClass: 'active'
 })
