b36610e7d0e205b52ba2d9923a2f5d9c10b765bb
stone <674656681@QQ.com>
2018-01-29 18:11:37 +0800

    not
    Signed-off-by: stone <674656681@QQ.com>

--- pc/src/routing/notFound.vue
+++ pc/src/routing/notFound.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="">
-     暂无页面
+  <div class="main-box not_ center" >
+     <img v-lazy="NOT" alt="">
+     <p class="hui-color">没有这个页面 -_-! </p>
   </div>
 </template>
 
 <script>
+import NOT from '@/assets/404.png'
 export default {
-
+  data(){
+    return{
+      NOT:NOT
+    }
+  }
 }
 </script>
-
-<style>
-
+<style lang="less" scoped>
+ .not_{
+  padding-bottom:100px;
+   img{
+      margin-top:150px;
+     display: inline-block;
+    vertical-align: middle;
+   }
+   p{
+      line-height:100px;
+   }
+ }
 </style>
+ 
